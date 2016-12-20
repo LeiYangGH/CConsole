3313d37fb902b3f5787d61f0e3b76620486e1ef3
Lei Yang <675686066@qq.com>
2016-12-20 20:23:00 +0800

    fix find by no

--- CStu/CStu/CStu/Source.cpp
+++ CStu/CStu/CStu/Source.cpp
@@ -232,14 +232,16 @@ int promptsearchtotalbyname()
 
 
 
-int searchtotalbyno(int no)
+void searchtotalbyno(int no)
 {
 	int i;
 	for (i = 0; i < allstudentscount; i++)
 		if (allstudents[i].no == no)
-			return allstudents[i].average;
+		{
+			displaystudent(allstudents[i]);
+			return;
+		}
 	printf("没找到对应学生的信息。\r\n");
-	return 0;
 }
 
 void promptsearchtotalbyno()
@@ -247,7 +249,7 @@ void promptsearchtotalbyno()
 	int no;
 	printf("请输入要查询的学号:");
 	scanf("%d", &no);
-	printf("学号%d的成绩为%d：", no, searchtotalbyno(no));
+	searchtotalbyno(no);
 }
 
 //输入成绩信息
