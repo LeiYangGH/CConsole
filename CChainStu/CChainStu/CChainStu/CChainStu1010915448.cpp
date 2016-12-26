4e69f797eed14a9e42b3fff02a6ab32300e46a26
LeiYangGH <675686066@qq.com>
2016-12-26 23:19:31 +0800

    CChainStu1010915448 can add

---
+++ CChainStu/CChainStu/CChainStu/CChainStu1010915448.cpp
@@ -0,0 +1,153 @@
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+#define MAX_STRLEN 20
+#define FORMAT "%s\t%s\t%.2f\r\n"
+#define MEMBERS stu.no, stu.name, stu.score[N]
+#define N 2
+//#define N 5
+typedef struct student
+{
+	char no[12];
+	char name[10];
+	float score[N + 1];  /*  其中最后一个元素用来保存平均成绩  */
+	struct student  *next;
+}student;
+
+char *scoresfile = "scores.txt";
+//student *head;
+
+//字符串转整数
+int toint(char *s)
+{
+	char *end;
+	return (int)strtol(s, &end, 10);
+}
+
+
+
+//显示一个成绩
+void displaystudent(student stu)
+{
+	printf(FORMAT, MEMBERS);
+	//printf("---------------------");
+	printf("\r\n");
+}
+
+
+//新增成绩
+void addstudent(student *head)
+{
+	int i, score;
+	float sum = 0;
+	student *p = head;
+	student *n;
+	char no[MAX_STRLEN];
+	char name[MAX_STRLEN] = "";
+	printf("请输入学生学号:");
+	scanf("%s", &no);
+	printf("\n请输入学生姓名:");
+	scanf("%s", name);
+	n = (student *)malloc(sizeof(student));
+	strcpy(n->no, no);
+	strcpy(n->name, name);
+	for (i = 0; i < N; i++)
+	{
+		printf("\n请输入第%d们课程成绩:", i + 1);
+		scanf("%d", &score);
+		n->score[i] = score;
+		sum += score;
+	}
+	n->score[N] = sum / N;
+	p->next = n;
+	n->next = NULL;
+	printf("\n学生%s信息添加成功!\n", name);
+}
+
+
+
+
+//http://blog.csdn.net/iwm_next/article/details/7450734
+void deletestudent(student *head, char * name)  //删除成绩
+{
+	student *p1 = head, *p2;
+	if (head == NULL)
+	{
+		printf("\n成绩为空!\n");
+		return;
+	}
+	while (strcmp(p1->name, name) != 0 && p1->next != NULL)
+	{
+		p2 = p1;
+		p1 = p1->next;
+	}
+	if (strcmp(p1->name, name) == 0)
+	{
+
+		if (p1 == head)
+			head = p1->next;
+		else
+		{
+			p2->next = p1->next;
+			free(p1);
+			printf("已删除姓名为%s的学生的成绩。\r\n", name);
+		}
+	}
+	else
+		printf("没找到姓名为%s的学生!\r\n", name);
+}
+
+
+
+
+void displayallstudents(student *head)  //输出所有学生信息
+{
+	student *p = head->next;
+
+	printf("所有学生成绩如下\r\n");
+	printf("序号    姓名    成绩\n");
+	while (p != NULL)
+	{
+		displaystudent(*p);
+		p = p->next;
+	}
+}
+
+
+
+int cmpfunc(const void * a, const void * b)   //成绩比较
+{
+	return ((student*)a)->score - ((student*)b)->score;
+}
+
+
+
+
+
+
+
+//void promptdeletebyname()  //按姓名删除
+//{
+//	char name[50] = "";
+//	inputname(name);
+//	deletestudent(name);
+//}
+
+
+int main()
+{
+	int choice = -1;
+	student *head;
+
+	head = (student *)malloc(sizeof(student));
+
+	//createsamplestudents();
+
+	addstudent(head);
+	displayallstudents(head);
+
+	system("pause");
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+edit:template.exe
+template.exe:template.cpp
+	gcc template.cpp -o"C:\G\CConsole\template.exe"
+
+
