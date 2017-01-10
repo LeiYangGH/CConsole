c47da1a8c49186d28e9b7b1c57c473eca484fb57
LeiYangGH <675686066@qq.com>
2017-01-10 22:49:08 +0800

    can add and display address

--- CChainAddress/CChainAddress/CChainAddress/Source.cpp
+++ CChainAddress/CChainAddress/CChainAddress/Source.cpp
@@ -3,18 +3,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <conio.h>
-#define FORMAT3DSD "%d\t%s\t%d\r\n"
-
-typedef struct student
+//#define FORMAT3DSD "%d\t%s\t%d\r\n"
+#define LINE  "\n------------------------\n"
+typedef struct address
 {
 	int no;
-	char name[50];
-	int score;
-	student *next;
-}student;
+	char name[30];//定义通讯录联系人姓名
+	char home[30];//定义联系人的家庭住址
+	char tel[30];//定义联系人的电话号码
+	char qq[20];//定义联系人的qq号码
+	char email[30];//定义联系人的邮箱地址
+	address *next;
+}address;
 
-char *scoresfile = "scores.txt";
-student *head;
+address *head;
 
 //字符串转整数
 //int toint(char *s)
@@ -23,54 +25,33 @@ student *head;
 //	return (int)strtol(s, &end, 10);
 //}
 
- 
+
 
 //显示一个成绩
-void displaystudent(student stu)
+void displayaddress(address add)
 {
-	printf(FORMAT3DSD, stu.no, stu.name, stu.score);
-	printf("\r\n");
+	printf("%d\t%s\t%s\t%s\t%s\t%s\t\n", add.no, add.name, add.home, add.tel, add.qq, add.email);
+	//printf("\r\n");
 }
 
 //读取所有成绩到链表
-void readallstudents()
+void readalladdresss()
 {
 	char line[200];
-	student *p1, *p2;
-	/*FILE *fp = fopen(scoresfile, "r");
-	if (fp == NULL)
-	{
-		printf("\nerror on open file!");
-		fp = fopen(scoresfile, "a+");
-		getchar();
-	}*/
+	address *p1, *p2;
 
-	p1 = p2 = (student *)malloc(sizeof(student));
+	p1 = p2 = (address *)malloc(sizeof(address));
 	head = p1;
-	//while (fgets(line, 1024, fp) != NULL)
-	//{
-	//	if (strlen(line) < 5)
-	//		break;
-	//	//读进来的行去掉末尾换行符，重要
-	//	//http://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
-	//	strtok(line, "\n");
-	//	getstudentfromline(line, p1);
-	//	if (head == NULL)
-	//		head = p1;
-	//	else
-	//		p2->next = p1;
-	//	p2 = p1;
-	//	p1 = (student*)malloc(sizeof(student));
-	//}
+
 	p2->next = NULL;
 }
 
-//输入成绩信息
-void inputname(char str[])
+//输入某种提示的字符串
+void inputstring(char str[], char *description)
 {
-	printf("请输入姓名:");
+	printf("请输入%s:", description);
 	scanf("%s", str);
-	printf("您输入的姓名为为 %s \r\n", str);
+	printf("您输入的%s为 %s \n", description, str);
 }
 
 int inputscore()
@@ -84,10 +65,10 @@ int inputscore()
 	return n;
 }
 //获取一个新的学生信息
-int getnewno(student **tail)
+int getnewno(address **tail)
 {
 	int newno = 0;
-	student *p = head;
+	address *p = head;
 	if (head->no > newno)
 		newno = head->no;
 	while (p->next != NULL)
@@ -104,37 +85,47 @@ int getnewno(student **tail)
 }
 
 //新增成绩
-void addstudent()
+void addaddress()
 {
 	int newno = 0, score;//新成绩编号，当前最大+1
-	student *p = head;
-	student *n;
-	char name[50] = "";
+	char name[30] = "";
+	char home[30] = "";
+	char tel[30] = "";
+	char qq[20] = "";
+	char email[30] = "";
+	address *p = head;
+	address *n;
 
-	inputname(name);
+	inputstring(name, "姓名");
+	inputstring(home, "家庭地址");
+	inputstring(tel, "手机号");
+	inputstring(qq, "qq号");
+	inputstring(email, "邮箱");
 
 	newno = getnewno(&p);
 
-	n = (student *)malloc(sizeof(student));
+	n = (address *)malloc(sizeof(address));
 
 	n->no = newno;
 	strcpy(n->name, name);
-	score = inputscore();
-	n->score = score;
+	strcpy(n->home, home);
+	strcpy(n->tel, tel);
+	strcpy(n->qq, qq);
+	strcpy(n->email, email);
 
 	p->next = n;
 	n->next = NULL;
-	printf("\n%s成绩%d添加成功!\n", name, score);
+	printf("\n%s的信息添加成功!\n", name);
 }
 
 
-bool insert(student *pHead, int front, char *name, int score)
+bool insert(address *pHead, int front, char *name, int score)
 {
 	int i = 0, newno;
-	student *_node = pHead;
-	student *pSwap;
-	student *pNew;
-	student *x;
+	address *_node = pHead;
+	address *pSwap;
+	address *pNew;
+	address *x;
 	newno = getnewno(&x);
 	if ((front < 1) && (NULL != _node))
 	{
@@ -145,11 +136,11 @@ bool insert(student *pHead, int front, char *name, int score)
 		_node = _node->next;
 		++i;
 	}
-	pNew = (student*)malloc(sizeof(student));
+	pNew = (address*)malloc(sizeof(address));
 
 	pNew->no = newno;
 	strcpy(pNew->name, name);
-	pNew->score = score;
+	//pNew->score = score;
 	pSwap = _node->next;
 	_node->next = pNew;
 	pNew->next = pSwap;
@@ -160,9 +151,9 @@ bool insert(student *pHead, int front, char *name, int score)
 
 
 //http://blog.csdn.net/iwm_next/article/details/7450734
-void deletestudent(char * name)  //删除成绩
+void deleteaddress(char * name)  //删除成绩
 {
-	student *p1 = head, *p2;
+	address *p1 = head, *p2;
 	if (head == NULL)
 	{
 		printf("\n成绩为空!\n");
@@ -191,10 +182,10 @@ void deletestudent(char * name)  //删除成绩
 
 
 
-void displaystudent(char * name)  //按姓名输出
+void displayaddress(char * name)  //按姓名输出
 {
 	int found = 0;
-	student *p = head;
+	address *p = head;
 
 	while (p != NULL)
 	{
@@ -205,7 +196,7 @@ void displaystudent(char * name)  //按姓名输出
 
 			printf("序号    姓名    成绩\n");
 
-			displaystudent(*p);
+			displayaddress(*p);
 		}
 		p = p->next;
 	}
@@ -213,10 +204,10 @@ void displaystudent(char * name)  //按姓名输出
 		printf("没找到名为%s的学生\r\n", name);
 }
 
-void addstudent(char * name)  //追加学生信息
+void addaddress(char * name)  //追加学生信息
 {
 	int found = 0;
-	student *p = head;
+	address *p = head;
 
 	while (p != NULL)
 	{
@@ -227,11 +218,11 @@ void addstudent(char * name)  //追加学生信息
 
 			printf("序号    姓名    成绩\n");
 
-			displaystudent(*p);
+			displayaddress(*p);
 			printf("更改学生成绩为：");
 			getchar();
-			scanf("%d", &p->score);
-			printf("修改成功，更改后的学生成绩是：%d。\n", p->score);
+			/*	scanf("%d", &p->score);
+				printf("修改成功，更改后的学生成绩是：%d。\n", p->score);*/
 		}
 		p = p->next;
 	}
@@ -240,10 +231,10 @@ void addstudent(char * name)  //追加学生信息
 }
 
 //********************其实可以考虑作为一个通用函数，但我懒得改其他函数了
-void searchbyname(char * name, student **f)  //根据名字查找学生
+void searchbyname(char * name, address **f)  //根据名字查找学生
 {
 	int found = 0;
-	student *p = head;
+	address *p = head;
 
 	while (p != NULL)
 	{
@@ -261,21 +252,20 @@ void searchbyname(char * name, student **f)  //根据名字查找学生
 	}
 }
 
-void displayallstudents()  //输出所有学生信息
+void displayalladdresss()  //输出所有学生信息
 {
-	student *p = head->next;
+	address *p = head->next;
 
 	printf("所有学生成绩如下\n");
-	printf("---------------------\n");
+	printf(LINE);
 
-	printf("序号    姓名    成绩\n");
+	printf("姓名%t住址%t手机号%tqq%t邮箱\n");
 	while (p != NULL)
 	{
-		displaystudent(*p);
+		displayaddress(*p);
 		p = p->next;
 	}
-	printf("---------------------\n");
-
+	printf(LINE);
 }
 
 void promptinsertbeforeno()  //按编号插入
@@ -285,7 +275,7 @@ void promptinsertbeforeno()  //按编号插入
 	printf("\n请输入要在哪个编号的学生之后插入?\n");
 	scanf("%d", &no);
 
-	inputname(name);
+	//inputstring(name);
 	score = inputscore();
 	if (insert(head, no, name, score))
 		printf("\n插入成功！\n");
@@ -296,8 +286,8 @@ void promptinsertbeforeno()  //按编号插入
 void promptsearchtotalbyname()  //按姓名查找
 {
 	char name[50] = "";
-	inputname(name);
-	displaystudent(name);
+	//inputstring(name);
+	displayaddress(name);
 }
 
 
@@ -305,19 +295,19 @@ void promptsearchtotalbyname()  //按姓名查找
 void promptdeletebyname()  //按姓名删除
 {
 	char name[50] = "";
-	inputname(name);
-	deletestudent(name);
+	//inputstring(name);
+	deleteaddress(name);
 }
 
- 
+
 
 int main()
 {
 	int choice = -1;
 
-	//createsamplestudents();
+	//createsampleaddresss();
 
-	readallstudents();
+	readalladdresss();
 
 	while (choice != 0)
 	{
@@ -333,7 +323,7 @@ int main()
 		switch (choice)
 		{
 		case '0':
-			//writeallstudents();
+			//writealladdresss();
 			printf("\n\n 你选择了退出: ");
 			fseek(stdin, 0, SEEK_END);
 			system("pause");
@@ -341,11 +331,11 @@ int main()
 			break;
 		case '1':
 			printf("\n\n你选择了 1\n");
-			addstudent();
+			addaddress();
 			break;
 		case '2':
 			printf("\n\n你选择了 2\n");
-			displayallstudents();
+			displayalladdresss();
 			break;
 		case '3':
 			printf("\n\n你选择了 3\n");
@@ -359,7 +349,7 @@ int main()
 			printf("\n\n你选择了 5\n");
 			promptinsertbeforeno();
 			break;
-		 
+
 		default:
 			printf("\n\n输入有误，请重选\n");
 			break;
