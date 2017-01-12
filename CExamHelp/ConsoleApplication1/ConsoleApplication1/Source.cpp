f5660595756822a8743e79b8346099f3a18ff034
LeiYangGH <675686066@qq.com>
2017-01-12 08:37:01 +0800

    3 - 1

--- CExamHelp/ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ CExamHelp/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,39 +1,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-int index(char a[], int n, int i)
-{
-	int m, j;
-	for (m = i, j = i + 1; j < n; j++)
-		if (a[j] < a[m])
-			m = j;
-	return m;
-}
+//int index(char a[], int n, int i)
+//{
+//	int m, j;
+//	for (m = i, j = i + 1; j < n; j++)
+//		if (a[j] < a[m])
+//			m = j;
+//	return m;
+//}
 
-void swap(char *p, char *q)
-{
-	char temp;
-	temp = *p;
-	*p = *q;
-	*q = temp;
-}
-
-void arrange(char a[], char n)
-{
-	int i;
-	for (i = 0; i <= n - 2; i++)
-	{
-		int m;
-		m = index(a, n, i);
-		if (m != i)swap(a + i, a + m);
-	}
-}
+//void swap(char *p, char *q)
+//{
+//	char temp;
+//	temp = *p;
+//	*p = *q;
+//	*q = temp;
+//}
+//
+//void arrange(char a[], char n)
+//{
+//	int i;
+//	for (i = 0; i <= n - 2; i++)
+//	{
+//		int m;
+//		m = index(a, n, i);
+//		if (m != i)swap(a + i, a + m);
+//	}
+//}
 
 int main()
 {
-	char s[] = "2017";
-	arrange(s, strlen(s));
-	puts(s);
+	int i, sum = 0;
+	for (i = 1; i < 4; i++)
+		sum += i*i;
+	printf("%d\n", sum);
 	system("pause");
 	return 0;
 }
