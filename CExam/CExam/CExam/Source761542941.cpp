f2b76de2b7bf1335c815e0d08460875a6b84bf79
LeiYangGH <675686066@qq.com>
2017-03-24 14:39:40 +0800

    promptaddquestion

--- CExam/CExam/CExam/Source761542941.cpp
+++ CExam/CExam/CExam/Source761542941.cpp
@@ -5,7 +5,7 @@
 #define TEST 0
 #define MAX_QUESTIONS_COUNT 98
 #define STUDENT_FILE "student.txt"
-#define FILE_SEL "test.txt"
+#define QUESTION_FILE "test.txt"
 #define RESULT_FILE "result.txt"
 #define ADMIN "admin"
 
@@ -29,7 +29,7 @@ typedef struct question
 
 
 question allquestions[100];
-int allquestioncnt = 0;
+int allquestionscount = 0;
 
 int streq(char *s1, char *s2)
 {
@@ -50,7 +50,6 @@ void login()
 	printf("您输入的用户名是:%s", username);
 }
 
-//---
 student getstudentfromline(char *line)
 {
 	char *part;
@@ -159,6 +158,76 @@ void promptaddstudent()
 	fseek(stdin, 0, SEEK_END);
 	addstudent(no, name, idcard);
 }
+//---
+void appendquestion(question re)
+{
+	FILE *fp = fopen(QUESTION_FILE, "a");
+	if (fp == NULL)
+	{
+		printf("\n打开文件%s失败!", QUESTION_FILE);
+		getchar();
+		exit(1);
+	}
+	fprintf(fp, "%s\t%s\t%s\t%s\t%s\t%d\r\n", re.title, re.choices[0], re.choices[1], re.choices[2], re.choices[3], re.best);
+	fclose(fp);
+	printf("已保存题库到文件。");
+}
+
+int checkquestionexists(char *title)
+{
+	int i;
+	for (i = 0; i < allquestionscount; i++)
+	{
+		if (streq(allquestions[i].title, title))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void addquestion(char *title, char *c1, char *c2, char *c3, char *c4, int best)
+{
+	int i;
+	question re;
+	char teamname[20] = "";
+	strcpy(re.title, title);
+	strcpy(re.choices[0], c1);
+	strcpy(re.choices[1], c2);
+	strcpy(re.choices[2], c3);
+	strcpy(re.choices[3], c4);
+	re.best = best;
+	allquestions[allquestionscount++] = re;
+	appendquestion(re);
+}
+void promptaddquestion()
+{
+	char title[20] = "";
+	char c1[20] = "";
+	char c2[20] = "";
+	char c3[20] = "";
+	char c4[20] = "";
+	int best = 0;
+	if (!streq(username, ADMIN))
+	{
+		printf("必须以管理员登录才能添加考生信息！");
+		return;
+	}
+	printf("请输入要添加考题标题（不带空格）回车结束:\n");
+	scanf("%s", title);
+	if (checkquestionexists(title))
+	{
+		printf("考题%s已存在不能重复添加。\n", title);
+		return;
+	}
+	printf("请依次输入四个选项（选项本身不带空格，选项之间以空格隔开）回车结束:\n");
+	scanf("%s%s%s%s", c1, c2, c3, c4);
+	printf("请输入答案序号（1～4）回车结束:");
+	scanf("%d", &best);
+	fseek(stdin, 0, SEEK_END);
+	addquestion(title, c1, c2, c3, c4, best);
+}
+
 //--//
 int checklogin()
 {
@@ -250,20 +319,20 @@ question getquestionfromline(char *line)
 void readallquestions()
 {
 	char line[200];
-	FILE *fp = fopen(FILE_SEL, "r");
+	FILE *fp = fopen(QUESTION_FILE, "r");
 	if (fp == NULL)
 	{
-		printf("\n打开文件%s失败!", FILE_SEL);
+		printf("\n打开文件%s失败!", QUESTION_FILE);
 		getchar();
 		exit(1);
 	}
-	allquestioncnt = 0;
+	allquestionscount = 0;
 
 	while (fgets(line, 1024, fp) != NULL)
 	{
 		if (strlen(line) < 5)
 			continue;
-		allquestions[allquestioncnt++] = getquestionfromline(line);
+		allquestions[allquestionscount++] = getquestionfromline(line);
 	}
 	printf("\n已读入所有考题文件!\n");
 
@@ -324,7 +393,7 @@ int testallquestions(int usequestionscount)
 {
 	int i, score = 0;
 	int useids[MAX_QUESTIONS_COUNT] = { 0 };
-	generateuseids(allquestioncnt, usequestionscount, useids);
+	generateuseids(allquestionscount, usequestionscount, useids);
 	printf("----------共%d题-----------\n\n", usequestionscount);
 	for (i = 0; i < usequestionscount; i++)
 	{
@@ -348,7 +417,7 @@ void inputcountandexam()
 		printf("当前登录用户%s没登记，请联系管理员。", username);
 		return;
 	}
-	printf("\n请输入要抽取的考题数量(2~%d)，并以回车结束:", allquestioncnt - 2);
+	printf("\n请输入要抽取的考题数量(2~%d)，并以回车结束:", allquestionscount - 2);
 	scanf("%d", &usecnt);
 	srand(time(NULL));
 	testallquestions(usecnt);
@@ -361,7 +430,7 @@ int main()
 {
 	readallstudents();
 	readallquestions();
-
+	//addquestion("t9", "c91", "c92", "c93", "c94", 1);
 #if TEST
 	//strcpy(username, "ly");
 	strcpy(username, "s7");
@@ -379,6 +448,7 @@ int main()
 		printf("\t 2. 学生考试\n");
 		printf("\t 3. 查看所有成绩\n");
 		printf("\t 4. 添加考生信息\n");
+		printf("\t 5. 添加考题\n");
 		printf("\t 0. 退出");
 		printf("\n\n 请选择: ");
 		fseek(stdin, 0, SEEK_END);
@@ -407,9 +477,10 @@ int main()
 			printf("\n\n你选择了 4\n");
 			promptaddstudent();
 			break;
-			//case '5':
-			//	printf("\n\n你选择了 5\n");
-			//	break;
+		case '5':
+			printf("\n\n你选择了 5\n");
+			promptaddquestion();
+			break;
 		default:
 			printf("\n\n输入有误，请重选\n");
 			break;
