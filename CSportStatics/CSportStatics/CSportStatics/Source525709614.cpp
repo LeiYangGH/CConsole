9d9a35a2dcacc9b3b434e61370ab7c7e916399e2
LeiYangGH <675686066@qq.com>
2017-05-17 19:57:36 +0800

    can read

--- CSportStatics/CSportStatics/CSportStatics/Source525709614.cpp
+++ CSportStatics/CSportStatics/CSportStatics/Source525709614.cpp
@@ -2,53 +2,56 @@
 #include <string.h>
 #include <stdio.h>
 #define FILE_INPUT "input.txt"
-#define FILE_ALLOUTPUT "alloutput.txt"
-#define FILE_GRADES "grades.txt"
+//#define FILE_ALLOUTPUT "alloutput.txt"
+//#define FILE_GRADES "grades.txt"
 #define MAX_STRLEN 20
-#define COURSES_COUNT 5
-#define GRADES_COUNT 5
-
+//#define COURSES_COUNT 5
+//#define GRADES_COUNT 5
+#define TEST 1
 typedef struct student
 {
 	char name[MAX_STRLEN];
-	int score[COURSES_COUNT];
-	float average;
-	int order;
+	int i1;
+	int i2;
+	int i3;
+	float f;
 }student;
 student allstudents[100];
 int allstudentscount = 0;
 
-typedef struct course
-{
-	int oldindex;
-	float average;
-	int countingrade[GRADES_COUNT];
-}course;
-course allcourses[COURSES_COUNT];
+//typedef struct course
+//{
+//	int oldindex;
+//	float average;
+//	int countingrade[GRADES_COUNT];
+//}course;
+//course allcourses[COURSES_COUNT];
 
 
-#define TEST 0
 int cmpcourseindex;
-//字符串转整数
 int toint(char *s)
 {
 	char *end;
 	return (int)strtol(s, &end, 10);
 }
 
+float tofloat(char *s)
+{
+	double temp = strtod(s, NULL);
+	return atof(s);
+}
+
 void displaystudent(student stu)
 {
 	printf("\r\n");
-	printf("%s\t%d\t%d\t%d\t%d\t%d\t%.1f\t%d\n", stu.name,
-		stu.score[0], stu.score[1], stu.score[2], stu.score[3], stu.score[4],
-		stu.average, stu.order);
+	printf("%16s\t%d\t%d\t%d\t%.2f\n", stu.name, stu.i1, stu.i2, stu.i3, stu.f);
 }
 
 void displayallstudents()
 {
 	int i;
 	printf("所有%d位同学分数如下\r\n", allstudentscount);
-	printf("姓名\t科目1\t科目2\t科目3\t科目4\t科目5\t平均\t排名\n");
+	printf("%16s\t科目1\t科目2\t科目3\t科目4\n", "姓名");
 	printf("--------------------------------------------\r\n");
 	for (i = 0; i < allstudentscount; i++)
 	{
@@ -57,65 +60,17 @@ void displayallstudents()
 	printf("\r\n--------------------------------------------\r\n");
 }
 
-void displayonecourseorder()
-{
-	int i;
-	printf("第%d科目分数如下\r\n", cmpcourseindex + 1);
-	printf("--------------------------------------------\r\n");
-	for (i = 0; i < allstudentscount; i++)
-	{
-		printf("\r\n");
-		student stu = allstudents[i];
-		printf("%s\t\%d\n", stu.name,
-			stu.score[cmpcourseindex]);
-	}
-	printf("\r\n--------------------------------------------\r\n");
-}
 
-void displayallcoursesaveorder()
-{
-	int i;
-	printf("所有科目平均分排序如下\r\n");
-	printf("--------------------------------------------\r\n");
-	for (i = 0; i < COURSES_COUNT; i++)
-	{
-		printf("\r\n");
-		course cou = allcourses[i];
-		printf("第%d科\t\%.1f\n", cou.oldindex + 1,
-			cou.average);
-	}
-	printf("\r\n--------------------------------------------\r\n");
-}
 
-void writestudentsaveorder()
-{
-	int i;
-	FILE *fp;
-	fp = fopen(FILE_ALLOUTPUT, "w");
-	if (fp == NULL)
-	{
-		printf("\n打开文件%s失败!", FILE_ALLOUTPUT);
-		getchar();
-		exit(1);
-	}
-	fprintf(fp, "姓名\t科目1\t科目2\t科目3\t科目4\t科目5\t平均\t排名\n");
-	for (i = 0; i < allstudentscount; i++)
-	{
-		student stu = allstudents[i];
-		fprintf(fp, "%s\t%d\t%d\t%d\t%d\t%d\t%.1f\t%d\n", stu.name,
-			stu.score[0], stu.score[1], stu.score[2], stu.score[3], stu.score[4],
-			stu.average, stu.order);
-	}
-	fclose(fp);
-	printf("学生综合成绩已输出到文件%s\n", FILE_ALLOUTPUT);
-}
+
+
 
 student getstudentfromline(char *line)
 {
 	char *part;
 	int index = 0;
 	student stu;
-	part = strtok(line, "\t");
+	part = strtok(line, "\t\n");
 	while (part != NULL)
 	{
 		switch (++index)
@@ -124,36 +79,34 @@ student getstudentfromline(char *line)
 			strcpy(stu.name, part);
 			break;
 		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-			stu.score[index - 2] = toint(part);
+			stu.i1 = toint(part);
 			break;
-
+		case 3:
+			stu.i2 = toint(part);
+			break;
+		case 4:
+			stu.i3 = toint(part);
+			break;
+		case 5:
+			stu.f = tofloat(part);
+			break;
 		default:
 			break;
 		}
-		part = strtok(NULL, "\t");
+		part = strtok(NULL, "\t\n");
 	}
 	return stu;
 }
 
-void calctotalandave()
+int cmpstuavefunc(const void * b, const void * a)
 {
-	int i, j;
-	float sum = 0;
-	for (i = 0; i < allstudentscount; i++)
-	{
-		sum = 0;
-		for (j = 0; j < COURSES_COUNT; j++)
-		{
-			sum += allstudents[i].score[j];
-		}
-		allstudents[i].average = sum / (float)COURSES_COUNT;
-	}
+	return (((student*)a)->i2 - ((student*)b)->i2);
 }
+void sortbyi2()
+{
+	qsort(allstudents, allstudentscount, sizeof(student), cmpstuavefunc);
 
+}
 
 void readallstudents()
 {
@@ -174,108 +127,14 @@ void readallstudents()
 		++allstudentscount;
 		allstudents[allstudentscount - 1] = getstudentfromline(line);
 	}
-	calctotalandave();
+	sortbyi2();
 }
 
 
 
-int cmpstuavefunc(const void * b, const void * a)
-{
-	return (((student*)a)->average - ((student*)b)->average);
-}
-void sortstuave()
-{
-	int i;
-	qsort(allstudents, allstudentscount, sizeof(student), cmpstuavefunc);
-	allstudents[0].order = 1;
-	for (i = 1; i < allstudentscount; i++)
-	{
-		if (allstudents[i].average == allstudents[i - 1].average)
-			allstudents[i].order = allstudents[i - 1].order;
-		else
-			allstudents[i].order = allstudents[i - 1].order + 1;
-	}
-}
-
-void countbygradesforonecourse(int courseindex)
-{
-	int i, t;
-	for (i = 0; i < allstudentscount; i++)
-	{
-		t = allstudents[i].score[courseindex];
-		if (t < 60)
-			allcourses[courseindex].countingrade[0]++;
-		else if (t >= 60 && t <= 69)
-			allcourses[courseindex].countingrade[1]++;
-		else if (t >= 70 && t <= 79)
-			allcourses[courseindex].countingrade[2]++;
-		else if (t >= 80 && t <= 89)
-			allcourses[courseindex].countingrade[3]++;
-		else if (t >= 90)
-			allcourses[courseindex].countingrade[4]++;
-	}
-}
-
-void countbygradesforallcourses()
-{
-	int i;
-	for (i = 0; i < COURSES_COUNT; i++)
-	{
-		countbygradesforonecourse(i);
-	}
-}
 
 
-void writeandprintgradescount()
-{
-	int i, j;
-	char *grades[GRADES_COUNT] = { "<60","60~69", "70~79", "80~89", ">=90", };
-	FILE *fp;
-	fp = fopen(FILE_GRADES, "w");
-	if (fp == NULL)
-	{
-		printf("\n打开文件%s失败!", FILE_GRADES);
-		getchar();
-		exit(1);
-	}
-	printf("分数段\t科目1\t科目2\t科目3\t科目4\t科目5\n");
-	fprintf(fp, "分数段\t科目1\t科目2\t科目3\t科目4\t科目5\n");
-	for (i = 0; i < GRADES_COUNT; i++)
-	{
-		printf("%s", grades[i]);
-		fprintf(fp, "%s", grades[i]);
-		for (j = 0; j < COURSES_COUNT; j++)
-		{
-			course cou = allcourses[j];
-			printf("\t%d", cou.countingrade[i]);
-			fprintf(fp, "\t%d", cou.countingrade[i]);
-		}
-		printf("\n");
-		fprintf(fp, "\n");
-	}
-	fclose(fp);
-	printf("分数段已输出到文件%s\n", FILE_GRADES);
-}
 
-void displayallbelow60()
-{
-	int i, j, t;
-	printf("所有不及格信息如下\r\n");
-	printf("--------------------------------------------\r\n");
-	for (i = 0; i < allstudentscount; i++)
-	{
-		for (j = 0; j < COURSES_COUNT; j++)
-		{
-			t = allstudents[i].score[j];
-			if (t < 60)
-			{
-				printf("%s\t科目%d\t%d分\n",
-					allstudents[i].name, j + 1, t);
-			}
-		}
-	}
-	printf("\r\n--------------------------------------------\r\n");
-}
 
 void writeallstudents()
 {
@@ -293,50 +152,50 @@ void writeallstudents()
 	for (i = 0; i < allstudentscount; i++)
 	{
 		stu = allstudents[i];
-		fprintf(fp, "%s\t%d\t%d\t%d\t%d\t%d\r\n", stu.name,
-			stu.score[0], stu.score[1], stu.score[2], stu.score[3], stu.score[4]);
+		fprintf(fp, "%s\t%d\t%d\t%d\t%.2f\n", stu.name, stu.i1, stu.i2, stu.i3, stu.f);
 	}
 	fclose(fp);
 	printf("已保存记录到文件。");
 }
 
-void addstudent(char no[], char name[], int s0, int s1, int s2, int s3, int s4)
+void addstudent(char name[], int i1, int i2, int i3, float f)
 {
 	student stu;
 	strcpy(stu.name, name);
-	stu.score[0] = s0;
-	stu.score[1] = s1;
-	stu.score[2] = s2;
-	stu.score[3] = s3;
-	stu.score[4] = s4;
+	stu.i1 = i1;
+	stu.i2 = i2;
+	stu.i3 = i3;
+	stu.f = f;
 	allstudents[allstudentscount++] = stu;
-	calctotalandave();
+	sortbyi2();
 	writeallstudents();
 }
 
-void promptaddstudent()
-{
-	char no[MAX_STRLEN];
-	char name[MAX_STRLEN] = "";
-	int s1, s2, s3, s4, s5;
-	printf("\n请输入学生姓名\n");
-	scanf("%s", name);
-	printf("\n请输入5科成绩（整数），空格隔开\n");
-	scanf("%d%d%d%d%d", &s1, &s2, &s3, &s4, &s5);
-	addstudent(no, name, s1, s2, s3, s4, s5);
-	printf("完成第%d个学生成绩录入!\r\n", allstudentscount);
-}
+//void promptaddstudent()
+//{
+//	char no[MAX_STRLEN];
+//	char name[MAX_STRLEN] = "";
+//	int s1, s2, s3, s4, s5;
+//	printf("\n请输入学生姓名\n");
+//	scanf("%s", name);
+//	printf("\n请输入5科成绩（整数），空格隔开\n");
+//	scanf("%d%d%d%d%d", &s1, &s2, &s3, &s4, &s5);
+//	addstudent(no, name, s1, s2, s3, s4, s5);
+//	printf("完成第%d个学生成绩录入!\r\n", allstudentscount);
+//}
 
 int main()
 {
+
 #if TEST
+
 	readallstudents();
 	//sorttotal();
-	//displayallstudents();
-	ascending = -1;
-	cmpcourseindex = 4;
-	sortonecourse();
-	displayonecourseorder();
+	displayallstudents();
+	//ascending = -1;
+	//cmpcourseindex = 4;
+	//sortonecourse();
+
 #else
 
 	int choice = -1;
