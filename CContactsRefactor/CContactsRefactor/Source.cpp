90dfbbc100a262856e67b743014996c0c4b06a8e
LeiYangGH <675686066@qq.com>
2017-05-30 16:12:17 +0800

    remove declarations

--- CContactsRefactor/CContactsRefactor/Source.cpp
+++ CContactsRefactor/CContactsRefactor/Source.cpp
@@ -20,82 +20,10 @@ typedef struct /*定义数据结构*/
 	char name[20]; /*姓名*/
 	char units[30];  /*单位*/
 	char tele[10];  /*电话*/
-}ADDRESS;
-
-int enter(ADDRESS t[]); /*输入记录*/
-void list(ADDRESS t[], int n); /*显示记录*/
-void search(ADDRESS t[], int n); /*按姓名查找显示记录*/
-int  dele(ADDRESS t[], int n); /*删除记录*/
-int  add(ADDRESS t[], int n); /*插入记录*/
-void save(ADDRESS t[], int n); /*记录保存为文件*/
-int load(ADDRESS t[]);  /*从文件中读记录*/
-void display(ADDRESS t[]); /*按序号查找显示记录*/
-void sort(ADDRESS t[], int n); /*按姓名排序*/
-void qseek(ADDRESS t[], int n); /*快速查找记录*/
-void copy();  /*文件复制*/
-void print(ADDRESS temp); /*显示单条记录*/
-int find(ADDRESS t[], int n, char *s); /*查找函数*/
-int menu_select();  /*主菜单函数*/
-void gotoxy(int x, int y);
-
-/******主函数开始*******/
-void main()
-{
-	ADDRESS adr[M];  /*定义结构体数组*/
-	int length;  /*保存记录长度*/
-	system("cls");  /*清屏*/
-	for (;;)/*无限循环*/
-	{
-		switch (menu_select())   /*调用主菜单函数，返回值整数作开关语句的条件*/
-		{
-		case 0:length = enter(adr); break;/*输入记录*/
-		case 1:list(adr, length); break; /*显示全部记录*/
-		case 2:search(adr, length); break; /*查找记录*/
-		case 3:length = dele(adr, length); break; /*删除记录*/
-		case 4:length = add(adr, length);  break;   /*插入记录*/
-		case 5:save(adr, length); break; /*保存文件*/
-		case 6:length = load(adr); break; /*读文件*/
-		case 7:display(adr); break;  /*按序号显示记录*/
-		case 8:sort(adr, length); break; /*按姓名排序*/
-		case 9:qseek(adr, length); break; /*快速查找记录*/
-		case 10:copy(); break; /*复制文件*/
-		case 11:exit(0); /*如返回值为11则程序结束*/
-		}
-	}
-}
-int menu_select()
-{
-	char s[80];
-	int c;
-	gotoxy(1, 25);/*将光标定为在第25行，第1列*/
-	printf("press any key enter menu......\n");/*提示压任意键继续*/
-	getchar(); /*读入任意字符*/
-	system("cls"); /*清屏*/
-	gotoxy(1, 1);
-	printf("********************MENU*********************\n\n");
-	printf("		0. Enter record\n");
-	printf("		1. List the file\n");
-	printf("		2. Search record on name\n");
-	printf("		3. Delete a record\n");
-	printf("		4. add record \n");
-	printf("		5. Save the file\n");
-	printf("		6. Load the file\n");
-	printf("		7. display record on order\n");
-	printf("	        8. sort to make new file\n");
-	printf("	        9. Quick seek record\n");
-	printf("		10. copy the file to new file\n");
-	printf("	        11. Quit\n");
-	printf("***********************************************\n");
-	do {
-		printf("\n	 Enter you choice(0~11):"); /*提示输入选项*/
-		scanf("%s", s); /*输入选择项*/
-		c = atoi(s); /*将输入的字符串转化为整型数*/
-	} while (c<0 || c>11); /*选择项不在0~11之间重输*/
-	return c; /*返回选择项，主程序根据该数调用相应的函数*/
-}
+}address;
 
 /*输入记录，形参为结构体数组，函数值返回类型为整型表示记录长度*/
-int  enter(ADDRESS t[]) {
+int  enter(address t[]) {
 	int i, n;
 	system("cls"); /*清屏*/
 	printf("\nplease input num \n"); /*提示信息*/
@@ -112,7 +40,7 @@ int  enter(ADDRESS t[]) {
 }
 
 /*显示记录，参数为记录数组和记录条数*/
-void list(ADDRESS t[], int n)
+void list(address t[], int n)
 {
 	int i;
 	system("cls");
@@ -129,8 +57,31 @@ void list(ADDRESS t[], int n)
 	printf("************************end*******************\n");
 }
 
+
+/*显示指定的一条记录*/
+void print(address temp)
+{
+	system("cls");
+	printf("\n\n********************************************\n");
+	printf("name                unit                      telephone\n");
+	printf("------------------------------------------------\n");
+	printf("%-20s%-30s%-10s\n", temp.name, temp.units, temp.tele);
+	printf("**********************end***********************\n");
+}
+/*查找函数，参数为记录数组和记录条数以及姓名s */
+int find(address t[], int n, char *s)
+{
+	int i;
+	for (i = 0; i<n; i++)/*从第一条记录开始，直到最后一条*/
+	{
+		if (strcmp(s, t[i].name) == 0)  /*记录中的姓名和待比较的姓名是否相等*/
+			return i;   /*相等，则返回该记录的下标号，程序提前结结束*/
+	}
+	return i;  /*返回i值*/
+}
+
 /*查找记录*/
-void search(ADDRESS t[], int n)
+void search(address t[], int n)
 {
 	char s[20];   /*保存待查找姓名字符串*/
 	int i;   /*保存查找到结点的序号*/
@@ -143,29 +94,9 @@ void search(ADDRESS t[], int n)
 	else
 		print(t[i]);  /*找到，调用显示函数显示记录*/
 }
-/*显示指定的一条记录*/
-void print(ADDRESS temp)
-{
-	system("cls");
-	printf("\n\n********************************************\n");
-	printf("name                unit                      telephone\n");
-	printf("------------------------------------------------\n");
-	printf("%-20s%-30s%-10s\n", temp.name, temp.units, temp.tele);
-	printf("**********************end***********************\n");
-}
-/*查找函数，参数为记录数组和记录条数以及姓名s */
-int find(ADDRESS t[], int n, char *s)
-{
-	int i;
-	for (i = 0; i<n; i++)/*从第一条记录开始，直到最后一条*/
-	{
-		if (strcmp(s, t[i].name) == 0)  /*记录中的姓名和待比较的姓名是否相等*/
-			return i;   /*相等，则返回该记录的下标号，程序提前结结束*/
-	}
-	return i;  /*返回i值*/
-}
+
 /*删除函数，参数为记录数组和记录条数*/
-int dele(ADDRESS t[], int n)
+int dele(address t[], int n)
 {
 	char s[20];  /*要删除记录的姓名*/
 	int ch = 0;
@@ -194,9 +125,9 @@ int dele(ADDRESS t[], int n)
 	return n;  /*返回记录数*/
 }
 /*插入记录函数，参数为结构体数组和记录数*/
-int add(ADDRESS t[], int n)/*插入函数，参数为结构体数组和记录数*/
+int add(address t[], int n)/*插入函数，参数为结构体数组和记录数*/
 {
-	ADDRESS temp;  /*新插入记录信息*/
+	address temp;  /*新插入记录信息*/
 	int i, j;
 	char s[20]; /*确定插入在哪个记录之前*/
 	printf("please input record\n");
@@ -221,7 +152,7 @@ int add(ADDRESS t[], int n)/*插入函数，参数为结构体数组和记录数*/
 	return n; /*返回记录数*/
 }
 /*保存函数，参数为结构体数组和记录数*/
-void save(ADDRESS t[], int n)
+void save(address t[], int n)
 {
 	int i;
 	FILE *fp;  /*指向文件的指针*/
@@ -242,7 +173,7 @@ void save(ADDRESS t[], int n)
 	printf("****save success***\n"); /*显示保存成功*/
 }
 /*读入函数，参数为结构体数组*/
-int load(ADDRESS t[])
+int load(address t[])
 {
 	int i, n;
 	FILE *fp; /*指向文件的指针*/
@@ -259,7 +190,7 @@ int load(ADDRESS t[])
 	return n; /*返回记录数*/
 }
 /*按序号显示记录函数*/
-void display(ADDRESS t[])
+void display(address t[])
 {
 	int id, n;
 	FILE *fp; /*指向文件的指针*/
@@ -273,7 +204,7 @@ void display(ADDRESS t[])
 	fscanf(fp, "%d", &n); /*从文件读入记录数*/
 	if (id >= 0 && id<n) /*判断序号是否在记录范围内*/
 	{
-		fseek(fp, (id - 1) * sizeof(ADDRESS), 1); /*移动文件指针到该记录位置*/
+		fseek(fp, (id - 1) * sizeof(address), 1); /*移动文件指针到该记录位置*/
 		print(t[id]); /*调用输出函数显示该记录*/
 		printf("\r\n");
 	}
@@ -282,10 +213,10 @@ void display(ADDRESS t[])
 	fclose(fp);  /*关闭文件*/
 }
 /*排序函数，参数为结构体数组和记录数*/
-void sort(ADDRESS t[], int n)
+void sort(address t[], int n)
 {
 	int i, j, flag;
-	ADDRESS temp; /*临时变量做交换数据用*/
+	address temp; /*临时变量做交换数据用*/
 	for (i = 0; i<n; i++)
 	{
 		flag = 0;  /*设标志判断是否发生过交换*/
@@ -308,7 +239,7 @@ void sort(ADDRESS t[], int n)
 	printf("sort sucess!!!\n"); /*显示排序成功*/
 }
 /*快速查找，参数为结构体数组和记录数*/
-void qseek(ADDRESS t[], int n)
+void qseek(address t[], int n)
 {
 	char s[20];
 	int l, r, m;
@@ -337,7 +268,7 @@ void copy()
 {
 	char outfile[20]; /*目标文件名*/
 	int i, n;
-	ADDRESS temp[M];  /*定义临时变量*/
+	address temp[M];  /*定义临时变量*/
 	FILE *sfp, *tfp; /*定义指向文件的指针*/
 	system("cls");/*清屏*/
 	if ((sfp = fopen("record.txt", "rb")) == NULL) /*打开记录文件*/
@@ -378,4 +309,61 @@ void gotoxy(int x, int y) //cursor an gewuenschte position auf dem bildschirm se
 	csbiInfo.dwCursorPosition.X = x; //cursorposition X koordinate festlegen 
 	csbiInfo.dwCursorPosition.Y = y; //cursorposition Y koordinate festlegen 
 	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition); //den cursor an die festgelegte koordinate setzen 
+}
+
+int menu_select()
+{
+	char s[80];
+	int c;
+	gotoxy(1, 25);/*将光标定为在第25行，第1列*/
+	printf("press any key enter menu......\n");/*提示压任意键继续*/
+	getchar(); /*读入任意字符*/
+	system("cls"); /*清屏*/
+	gotoxy(1, 1);
+	printf("********************MENU*********************\n\n");
+	printf("		0. Enter record\n");
+	printf("		1. List the file\n");
+	printf("		2. Search record on name\n");
+	printf("		3. Delete a record\n");
+	printf("		4. add record \n");
+	printf("		5. Save the file\n");
+	printf("		6. Load the file\n");
+	printf("		7. display record on order\n");
+	printf("	        8. sort to make new file\n");
+	printf("	        9. Quick seek record\n");
+	printf("		10. copy the file to new file\n");
+	printf("	        11. Quit\n");
+	printf("***********************************************\n");
+	do {
+		printf("\n	 Enter you choice(0~11):"); /*提示输入选项*/
+		scanf("%s", s); /*输入选择项*/
+		c = atoi(s); /*将输入的字符串转化为整型数*/
+	} while (c<0 || c>11); /*选择项不在0~11之间重输*/
+	return c; /*返回选择项，主程序根据该数调用相应的函数*/
+}
+
+/******主函数开始*******/
+void main()
+{
+	address adr[M];  /*定义结构体数组*/
+	int length;  /*保存记录长度*/
+	system("cls");  /*清屏*/
+	for (;;)/*无限循环*/
+	{
+		switch (menu_select())   /*调用主菜单函数，返回值整数作开关语句的条件*/
+		{
+		case 0:length = enter(adr); break;/*输入记录*/
+		case 1:list(adr, length); break; /*显示全部记录*/
+		case 2:search(adr, length); break; /*查找记录*/
+		case 3:length = dele(adr, length); break; /*删除记录*/
+		case 4:length = add(adr, length);  break;   /*插入记录*/
+		case 5:save(adr, length); break; /*保存文件*/
+		case 6:length = load(adr); break; /*读文件*/
+		case 7:display(adr); break;  /*按序号显示记录*/
+		case 8:sort(adr, length); break; /*按姓名排序*/
+		case 9:qseek(adr, length); break; /*快速查找记录*/
+		case 10:copy(); break; /*复制文件*/
+		case 11:exit(0); /*如返回值为11则程序结束*/
+		}
+	}
 }
