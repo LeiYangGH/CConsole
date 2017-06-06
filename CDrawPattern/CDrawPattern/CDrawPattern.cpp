c6023085f6d1690cc8e2e77e6fb81425fc72d4bd
Lei, Yang <yang_lei@mcafee.com>
2017-06-06 14:03:36 +0800

    tuan

--- CDrawPattern/CDrawPattern/CDrawPattern.cpp
+++ CDrawPattern/CDrawPattern/CDrawPattern.cpp
@@ -18,12 +18,38 @@ void jiaochayuan(int x, int y)
 	setorigin(0, 0);
 }
 
+/* 画出花朵的函数 */
+/* x和y为花心的坐标 */
+void tuan(int a, int b)
+{
+	int n = 15, i, j, r = 110, x[50], y[50];
+	float t;
+	setorigin(a, b);//重置原点坐标
+	setcolor(RED);
+	t = 3.14 * 2 / n;
+	for (i = 0; i<n; i++)//圆周上各个等分点的坐标 
+	{
+		x[i] = r*cos(i*t);
+		y[i] = r*sin(i*t);
+	}
+	for (i = 0; i<n - 1; i++)
+	{
+		for (j = i + 1; j<n; j++)
+		{
+			line(x[i], y[i], x[j], y[j]);
+		}
+	}
+
+}
+
+
 int main()
 {
 	initgraph(600, 600);
 	setbkcolor(RGB(255, 153, 51));
 	cleardevice();
-	jiaochayuan(300, 300);
+	//jiaochayuan(300, 300);
+	tuan(400, 400);
 	system("pause");
 	closegraph();
 	return 0;
