3397164f4272f3e7eae6e056bb7b1fc79ca3902e
LeiYangGH <675686066@qq.com>
2017-06-11 11:57:18 +0800

    vairable names

--- CSortAndStars/CSortAndStars/CSortAndStars.cpp
+++ CSortAndStars/CSortAndStars/CSortAndStars.cpp
@@ -46,17 +46,17 @@ void bubble_sort(int arr[], int len)
 //http://blog.csdn.net/u014136713/article/details/50879476
 //http://c.biancheng.net/cpp/html/2744.html
 //二分查找，返回的是找到的下标，没找到则为-1
-int binarySearch(int a[], int len, int key)
+int binarySearch(int arr[], int len, int find)
 {
 	int low = 0;
 	int high = len - 1;
 	while (low <= high)
 	{
 		int mid = (low + high) / 2;
-		int midVal = a[mid];
-		if (midVal < key)
+		int midVal = arr[mid];
+		if (midVal < find)
 			low = mid + 1;
-		else if (midVal > key)
+		else if (midVal > find)
 			high = mid - 1;
 		else
 			return mid;
@@ -66,19 +66,19 @@ int binarySearch(int a[], int len, int key)
 
 //http://www.tuicool.com/articles/ZRfi22J
 //按原顺序插入
-void insert(int arr[], int len, int num)
+void insert(int arr[], int len, int find)
 {
 	int i, j, t1, t2;
 	int end = arr[len - 2];
-	if (num >= end)
-		arr[len - 1] = num;
+	if (find >= end)
+		arr[len - 1] = find;
 	else
 	{
 		for (i = 0; i < len - 1; i++)
-			if (arr[i] > num)
+			if (arr[i] > find)
 			{
 				t1 = arr[i]; //先保存当前位置的值
-				arr[i] = num; //插入数值
+				arr[i] = find; //插入数值
 				for (j = i + 1; j < len; j++)
 				{ //以下三行将插入位置之后的数依次向后移动
 					t2 = arr[j];
@@ -91,10 +91,10 @@ void insert(int arr[], int len, int num)
 }
 
 //判断是否存在，不存在则插入
-void insertinorder(int arr[], int len, int key, int *len1)
+void insertinorder(int arr[], int len, int find, int *len1)
 {
 	//如果已经有这个数字，则不增加长度
-	if (binarySearch(arr, len, key) >= 0)
+	if (binarySearch(arr, len, find) >= 0)
 	{
 		*len1 = len;
 	}
@@ -102,23 +102,23 @@ void insertinorder(int arr[], int len, int key, int *len1)
 	{
 		//否则长度增加1
 		*len1 = len + 1;
-		insert(arr, *len1, key);
+		insert(arr, *len1, find);
 	}
 }
 
 //http://c.biancheng.net/cpp/html/2810.html
 //画杨辉三角
-void drawpyramid(int n)
+void drawpyramid(int layer)
 {
 	int i, j, k, a[14][14];
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= layer; i++)
 		a[i][1] = a[i][i] = 1;  /*两边的数令它为1，因为现在循环从1开始，就认为a[i][1]为第一个数*/
-	for (i = 3; i <= n; i++)
+	for (i = 3; i <= layer; i++)
 		for (j = 2; j <= i - 1; j++)
 			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];  /*除两边的数外都等于上两顶数之和*/
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= layer; i++)
 	{
-		for (k = 1; k <= n - i; k++)
+		for (k = 1; k <= layer - i; k++)
 			printf("   ");  /*这一行主要是在输出数之前打上空格占位，让输出的数更美观*/
 		for (j = 1; j <= i; j++)  /*j<=i的原因是不输出其它的数，只输出我们想要的数*/
 			printf("%6d", a[i][j]);
@@ -135,16 +135,20 @@ int main()
 	len1 = len;
 	printf("排序前：");
 	print_arr(arr, len);
+
 	bubble_sort(arr, len);
 	printf("排序后：");
 	print_arr(arr, len);
+
 	printf("请输入要查找和插入的整数：");
 	scanf("%d", &find);
 	insertinorder(arr, len, find, &len1);
 	printf("插入后：");
 	print_arr(arr, len1);
+
 	printf("输出图形：");
 	drawpyramid(10);
+
 	system("pause");
 	return 0;
 }
