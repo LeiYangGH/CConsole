9f9352f5b80e64eb8b678b7f989e0d440e7f325b
LeiYangGH <675686066@qq.com>
2017-08-16 00:11:48 -0700

    stdarg

--- ctest/t.c
+++ ctest/t.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 #include"assert.h"
+
+#include<stdarg.h>
+
+int add(int num,...) 
+{
+    va_list valist; 
+    int sum = 0;
+    int i;
+
+    va_start(valist, num);
+
+    for (i = 0; i < num; i++)
+    {
+       sum += va_arg(valist, int);
+    }
+    va_end(valist);
+
+    return sum;
+}
+
 int main()
 {
     extern int ext;
-    assert(ext==51);
+    assert(ext==5);
+
     printf("ext = %d\n",ext);
+    int sum = add(2,3,7);
+
+    printf("sum = %d\n",sum);
     printf("end");
 
 }
